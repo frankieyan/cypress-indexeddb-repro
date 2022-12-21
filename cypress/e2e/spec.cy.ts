dfb99b857c904b33c01c6ec8128717c75d54c7bd
Frankie Yan <frankieyan@gmail.com>
2022-12-21 10:30:14 -0800

    Increase timeout when reading indexedDB

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,7 +2,7 @@ describe('empty spec', () => {
   it('passes', () => {
     cy.visit('https://google.ca')
 
-    cy.window().then((win) => {
+    cy.window().then({ timeout: 12000 }, (win) => {
       return win.indexedDB.databases().then(db => {
         cy.log(db.toString())
       })
